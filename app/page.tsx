4368cf8453b4d5250d0c9719d653be44f9e4116b
nc-michael-rombi <michael.rombi@northcoders.com>
2024-04-24 19:39:07 +0100

    add invitation to discover quokkas

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,11 @@ export default function Home() {
       </Head>
       <main className="container mx-auto py-10 px-4 flex flex-col items-center justify-center">
         <h1 className="text-4xl font-bold mb-8 text-white">MovieGram</h1>
+        <p className="text-xl font-bold mb-8 text-white text-center">
+          If a movie has no picture a random mythical creature might appear.
+          <br />
+          Will you be able to find it?
+        </p>
       </main>
       <MovieSearch />
     </div>
